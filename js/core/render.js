b73c2ff2f3a229ccef28c865f88cfb102d0629db
feebberg <ferbgames1@outlook.com>
2026-05-14 14:29:52 -0600

    Update render.js

--- js/core/render.js
+++ js/core/render.js
@@ -1,1 +1,21 @@
+export const APEX_RENDER = {
+    init(games) {
+        const grid = document.getElementById("game-grid");
+        grid.innerHTML = "";
+
+        games.forEach(game => {
+            const card = document.createElement("div");
+            card.className = "game-card";
+
+            card.innerHTML = `
+                <img src="${game.thumbnail}">
+                <div class="title">${game.name}</div>
+            `;
+
+            card.onclick = () => window.APEX.launch.open(game);
+
+            grid.appendChild(card);
+        });
+    }
+};
 
