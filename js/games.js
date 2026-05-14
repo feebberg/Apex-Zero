3f4013cbd71bac2689f05e5f7b4fb49c9b8e85de
feebberg <ferbgames1@outlook.com>
2026-05-14 14:17:38 -0600

    Delete js/games.js

--- js/games.js
+++
@@ -1,8 +0,0 @@
-window.APEX_MANUAL_GAMES = [
-    {
-        id: "example-game",
-        title: "Example Game",
-        thumb: "assets/example-thumb.png",
-        path: "games/example-game/index.html"
-    }
-];
