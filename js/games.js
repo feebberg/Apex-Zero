3cc2ea36e663e5a6e145c2446e1ce5d34af5a7dc
feebberg <ferbgames1@outlook.com>
2026-05-14 14:18:54 -0600

    Create games.js

---
+++ js/games.js
@@ -0,0 +1,16 @@
+export const GAMES = [
+    {
+        id: "2048",
+        name: "2048",
+        category: "Puzzle",
+        url: "games/2048/index.html",
+        thumbnail: "assets/thumbs/2048.png"
+    },
+    {
+        id: "slope",
+        name: "Slope",
+        category: "Arcade",
+        url: "games/slope/index.html",
+        thumbnail: "assets/thumbs/slope.png"
+    }
+];
